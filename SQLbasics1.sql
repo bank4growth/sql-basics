cc08acaad04ea765cd30f9bd89840eaee28cbedf
bank4growth <bankingforgrowth@gmail.com>
2026-09-02 07:16:57 -0500

    2nd commit. Saving challenges 5 to 7

--- SQLbasics1.sql
+++ SQLbasics1.sql
@@ -1,3 +1,5 @@
+-- -- DRILLS SQL -- --
+-- BLOQUE 1, Base --
 SELECT count(*) FROM pagos
 
 PRAGMA table_info(pagos);
@@ -7,6 +9,7 @@ SELECT COUNT(*), COUNT(DISTINCT p.pago_id)
 FROM clientes c
 JOIN prestamos pr ON c.cliente_id = pr.cliente_id
 JOIN pagos p ON pr.prestamo_id = p.prestamo_id;
+
 SELECT COUNT(*)
 FROM clientes c
 JOIN prestamos pr ON c.cliente_id = pr.cliente_id
@@ -86,5 +89,86 @@ LIMIT 10;
 SELECT COUNT(*) FROM clientes;
 
 SELECT COUNT(ingreso_mensual) FROM clientes;
+
+SELECT
+  COUNT(*) AS total,
+  COUNT(ingreso_mensual) AS no_null,
+  SUM(CASE WHEN ingreso_mensual IS NULL THEN 1 ELSE 0 END) AS son_null,
+  SUM(CASE WHEN ingreso_mensual = 0 THEN 1 ELSE 0 END) AS son_cero
+FROM clientes;
+
+-- BLOQUE 2. Joins y la trampa del LEFT JOIN --
+-- reto 5
+SELECT 
+  c.segmento,
+  COUNT(pr.prestamo_id) AS cantidad_préstamos, 
+  SUM(CASE WHEN c.cliente_id IS NULL THEN 1 ELSE 0 END) AS prest_sin_clie
+FROM prestamos pr
+LEFT JOIN clientes c ON pr.cliente_id = c.cliente_id
+GROUP BY c.segmento;
+
+SELECT COUNT(*) FROM prestamos;
+
+SELECT c.segmento, COUNT(pr.prestamo_id) AS cantidad_préstamos
+FROM prestamos pr
+LEFT JOIN clientes c ON pr.cliente_id = c.cliente_id
+GROUP BY c.segmento;
+
+-- reto 6
+SELECT 
+  COUNT(CASE WHEN pr.cliente_id NOT EXISTS THEN 1 ELSE 0 END) AS clie_sin_préstamos
+FROM clientes c
+LEFT JOIN prestamos pr ON c.cliente_id = pr.cliente_id;
+
+SELECT c.cliente_id
+WHERE pr.prestamo_id IS NULL
+FROM clientes c
+LEFT JOIN prestamos pr ON c.cliente_id = pr.cliente_id;
+
+SELECT c.cliente_id
+FROM clientes c
+LEFT JOIN prestamos pr ON c.cliente_id = pr.cliente_id
+WHERE pr.prestamo_id IS NULL;
+
+SELECT COUNT(c.cliente_id) AS clie_sin_préstamos
+FROM clientes c
+WHERE NOT EXISTS (SELECT 1 FROM prestamos pr WHERE pr.cliente_id = c.cliente_id);
+
+SELECT c.cliente_id
+FROM clientes c
+WHERE NOT EXISTS (SELECT 1 FROM prestamos pr WHERE pr.cliente_id = c.cliente_id);
+
+-- reto 7
+SELECT estado, COUNT(estado) AS Tipos_Est
+from prestamos
+GROUP by estado
+order by Tipos_Est DESC;
+
+
+SELECT c.segmento, COUNT(p.prestamo_id) AS n_prestamos
+FROM clientes c
+LEFT JOIN prestamos p ON c.cliente_id = p.cliente_id
+WHERE p.estado = 'Vigente'
+GROUP BY c.segmento;
+
+--solución 1
+SELECT c.segmento, COUNT(p.prestamo_id) AS n_prestamos
+FROM clientes c
+FULL OUTER JOIN prestamos p ON c.cliente_id = p.cliente_id
+WHERE p.estado = 'Vigente'
+GROUP BY c.segmento
+ORDER BY n_prestamos DESC;
+
+--solución 2
+SELECT c.segmento, COUNT(p.prestamo_id) AS n_prestamos
+FROM prestamos p
+LEFT JOIN clientes c ON c.cliente_id = p.cliente_id
+WHERE p.estado = 'Vigente'
+GROUP BY c.segmento
+ORDER BY n_prestamos DESC;
+
+-- BLOQUE 3. Agregación de negocio --
+--Reto 8
+
 
 
